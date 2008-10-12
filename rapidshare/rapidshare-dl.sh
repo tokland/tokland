81c7c163a018a8621c23a83225aa1e2b128f8ab3
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2008-10-12 12:56:34 +0000

    - safety: xargs -r - use < FILE instead of cat FILE - match_line: use sed instead of grep + perl

--- rapidshare/rapidshare-dl.sh
+++ rapidshare/rapidshare-dl.sh
@@ -2,15 +2,17 @@
 #
 # Download files from Rapidshare.
 #
+# Depends: bash, mktemp, wget.
+#
 # Author: <tokland@gmail.com>.
 #
 set -e
 
-# Match a Perl-style regular expression for a grepped line.
+# Get first line that matches a regular expression and parse it.
 #
-# $1: POSIX-regexp to get the first line that matches.
-# $2: Perl-regexp to match (using parentheses) on the line.
-match_line() { grep "$1" | head -n1 | perl -nle "print m/$2/"; }
+# $1: POSIX-regexp to filter (get only the first matching line).
+# $2: POSIX-regexp to match (using parentheses) on the matched line.
+match_line() { sed -n "/$1/ s/^.*$2.*$/\1/p" | head -n1; }
 
 # Echo text to standard error.
 debug() { echo "$@" >&2; }
@@ -23,21 +25,21 @@ get_rapidshare_url() {
     trap "rm -f \$TEMPFILE" SIGINT SIGHUP SIGTERM
     TEMPFILE=$(mktemp)
     while true; do
-        WAIT_URL=$(wget -O - "$URL" | match_line '<form' 'action="(.*?)"')
-        test "$WAIT_URL" || { debug "can't get wait main page URL"; return 2; }
+        WAIT_URL=$(wget -O - "$URL" | match_line '<form' 'action="\(.*\)"')
+        test "$WAIT_URL" || { debug "can't get wait-page URL"; return 2; }
         wget -O $TEMPFILE --post-data="dl.start=Free" "$WAIT_URL"
-        LIMIT=$(cat $TEMPFILE | match_line "try again in about" "about (\d+) minut")
+        LIMIT=$(match_line "try again" "about \([[:digit:]]\+\) min" < $TEMPFILE)
         if [ -z "$LIMIT" ]; then break; fi
         debug "download limit reached: waiting $LIMIT minutes"
         sleep ${LIMIT}m
     done
-    FILE_URL=$(cat $TEMPFILE | match_line "<form " "action=\"(.*?)\"") 
-    SLEEP=$(cat $TEMPFILE | match_line "^var c=" "c=(\d+);")
+    FILE_URL=$(match_line "<form " 'action="\(.*\)"' < $TEMPFILE) 
+    SLEEP=$(match_line "^var c=" "c=\([[:digit:]]\+\);" < $TEMPFILE)
     rm -f $TEMPFILE
-    trap SIGINT SIGHUP SIGTERM
-    test "$FILE_URL"  || { debug "can't get file URL"; return 3; }
+    trap - SIGINT SIGHUP SIGTERM
+    test "$FILE_URL" || { debug "can't get file URL"; return 3; }
     debug "URL File: $FILE_URL" 
-    test "$SLEEP"  || { debug "can't get sleep time"; SLEEP=100; }
+    test "$SLEEP" || { debug "can't get sleep time"; SLEEP=100; }
     debug "Waiting $SLEEP seconds" 
     sleep $SLEEP
     echo $FILE_URL
@@ -46,18 +48,18 @@ get_rapidshare_url() {
 # Main
 
 if [ $# -eq 0 ]; then
-    debug "usage: $0 URL|FILE [...]"
+    debug "usage: $(basename $0) URL|FILE [URL|FILE ...]"
     exit 1
 fi
 
 for ITEM in "$@"; do
     if [ $(expr match "$ITEM" "http://") == 0 ]; then
-        # Item is a file path (it should be one link per file)
-        cat $ITEM | while read URL; do 
-            get_rapidshare_url $URL | xargs wget
-        done
+        # Item is not an URL, may be a file path (expect one link per file)
+        while read URL; do 
+            get_rapidshare_url "$URL" | xargs -r wget
+        done < "$ITEM"
     else
         # Item is a Rapidshare URL, download directly
-        get_rapidshare_url $ITEM | xargs wget
+        get_rapidshare_url "$ITEM" | xargs -r wget
     fi
 done
