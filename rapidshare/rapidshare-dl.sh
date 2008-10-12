df76f61a664aa1ef6ebb2e05daf522f5ee6797cb
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2008-10-12 16:40:14 +0000

    alexander.huemer gave some advice:
    - don't use temporal file
    - sleep + 1 sec

--- rapidshare/rapidshare-dl.sh
+++ rapidshare/rapidshare-dl.sh
@@ -2,8 +2,6 @@
 #
 # Download files from Rapidshare.
 #
-# Depends: bash, mktemp, wget.
-#
 # Author: <tokland@gmail.com>.
 #
 set -e
@@ -22,26 +20,22 @@ debug() { echo "$@" >&2; }
 # $1: A rapidshare URL
 get_rapidshare_url() {
     URL=$1
-    trap "rm -f \$TEMPFILE" SIGINT SIGHUP SIGTERM
-    TEMPFILE=$(mktemp)
     while true; do
         WAIT_URL=$(wget -O - "$URL" | match_line '<form' 'action="\(.*\)"')
         test "$WAIT_URL" || { debug "can't get wait-page URL"; return 2; }
-        wget -O $TEMPFILE --post-data="dl.start=Free" "$WAIT_URL"
-        LIMIT=$(match_line "try again" "about \([[:digit:]]\+\) min" < $TEMPFILE)
+        DATA=$(wget -O - --post-data="dl.start=Free" "$WAIT_URL")
+        LIMIT=$(echo "$DATA" | match_line "try again" "about \([[:digit:]]\+\) min")
         if [ -z "$LIMIT" ]; then break; fi
         debug "download limit reached: waiting $LIMIT minutes"
         sleep ${LIMIT}m
     done
-    FILE_URL=$(match_line "<form " 'action="\(.*\)"' < $TEMPFILE) 
-    SLEEP=$(match_line "^var c=" "c=\([[:digit:]]\+\);" < $TEMPFILE)
-    rm -f $TEMPFILE
-    trap - SIGINT SIGHUP SIGTERM
+    FILE_URL=$(echo "$DATA" | match_line "<form " 'action="\(.*\)"') 
+    SLEEP=$(echo "$DATA" | match_line "^var c=" "c=\([[:digit:]]\+\);")
     test "$FILE_URL" || { debug "can't get file URL"; return 3; }
     debug "URL File: $FILE_URL" 
     test "$SLEEP" || { debug "can't get sleep time"; SLEEP=100; }
     debug "Waiting $SLEEP seconds" 
-    sleep $SLEEP
+    sleep $(($SLEEP + 1))
     echo $FILE_URL
 }
 
@@ -54,7 +48,7 @@ fi
 
 for ITEM in "$@"; do
     if [ $(expr match "$ITEM" "http://") == 0 ]; then
-        # Item is not an URL, may be a file path (expect one link per file)
+        # Item is not an URL, treat as file path (expecting one link per file)
         while read URL; do 
             get_rapidshare_url "$URL" | xargs -r wget
         done < "$ITEM"
