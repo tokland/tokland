3e75a16b98775b138d3281db4ac80aebf82e5702
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2008-10-20 17:08:39 +0000

    details

--- rapidshare/rapidshare-dl.sh
+++ rapidshare/rapidshare-dl.sh
@@ -15,7 +15,7 @@ match_line() { sed -n "/$1/ s/^.*$2.*$/\1/p" | head -n1; }
 # Echo text to standard error.
 debug() { echo "$@" >&2; }
 
-# Get a rapidshare file download url
+# Output a rapidshare file download url given its Rapidshare URL
 #
 # $1: A rapidshare URL
 get_rapidshare_url() {
@@ -29,13 +29,13 @@ get_rapidshare_url() {
         debug "download limit reached: waiting $LIMIT minutes"
         sleep ${LIMIT}m
     done
-    FILE_URL=$(echo "$DATA" | match_line "<form " 'action="\(.*\)"') 
+    FILE_URL=$(echo "$DATA" | match_line "<form " 'action="\([^\"]*\)"') 
     SLEEP=$(echo "$DATA" | match_line "^var c=" "c=\([[:digit:]]\+\);")
     test "$FILE_URL" || { debug "can't get file URL"; return 3; }
     debug "URL File: $FILE_URL" 
     test "$SLEEP" || { debug "can't get sleep time"; SLEEP=100; }
     debug "Waiting $SLEEP seconds" 
-    sleep $(($SLEEP + 1))
+    sleep $(($SLEEP + 1)) # don't be greedy, wait a little more than specified
     echo $FILE_URL
 }
 
@@ -48,12 +48,12 @@ fi
 
 for ITEM in "$@"; do
     if [ $(expr match "$ITEM" "http://") == 0 ]; then
-        # Item is not an URL, treat as file path (expecting one link per file)
+        # Not an URL, assume it is a filename containing one link per line.
         while read URL; do 
             get_rapidshare_url "$URL" | xargs -r wget
         done < "$ITEM"
     else
-        # Item is a Rapidshare URL, download directly
+        # It's a URL, assume it is a Rapidshare URL 
         get_rapidshare_url "$ITEM" | xargs -r wget
     fi
 done
