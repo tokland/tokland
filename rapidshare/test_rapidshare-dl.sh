7dfc1459e60caeb3ae71aa2a374af558b0359aa7
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2009-01-06 09:48:28 +0000

    unit test

--- rapidshare/test_rapidshare-dl.sh
+++ rapidshare/test_rapidshare-dl.sh
@@ -7,39 +7,54 @@
 
 set -e
 
-# Build lines of filenames (only basename)
-build() {
- echo "$@" | xargs -n1 basename
-}
-
 # Check that $1 is equal to $2.
 assert_equal() {
   if ! test "$1" = "$2"; then
     echo "assert_equal failed: $1 != $2"
     return 1
-  else
-    echo "assert_equal ok"
   fi
-} 
+}
 
-# Real links (small files) 
+# Run a test
+# $1..N: Function to run
+run() {
+  echo -n "$NAME... "
+  "$@" && echo " ok" || echo " failed!"
+}
+
+### Tests
+ 
 URL1="http://www.rapidshare.com/files/86545320/Tux-Trainer_25-01-2008.rar"
 URL2="http://www.rapidshare.com/files/104938027/Fresh_RAM_4.5.0_tux.rar"
 URL3="http://www.rapidshare.com/files/75209315/TUX.rar"
-
 RS="./rapidshare-dl.sh"
 
+# Build lines of filenames (only basename)
+build() {
+ echo "$@" | xargs -n1 basename
+}
+
 # Single URL download
-assert_equal "$(build $URL1)" "$($RS $URL1)"        
-    
-# File listing download
-FILECONTENTS="$URL1
+test_single_url() {
+  assert_equal "$(build $URL1)" "$($RS $URL1)"
+}        
+
+test_file_listing() {    
+  # File listing download
+  FILECONTENTS="$URL1
 # a comment (followed by a empty line)
 
 $URL2"
-assert_equal "$(build $URL1 $URL2)" "$($RS <(echo "$FILECONTENTS"))" 
+  assert_equal "$(build $URL1 $URL2)" "$($RS <(echo "$FILECONTENTS"))"
+}
 
-# Mixed URL and file listing downloading  
-FILECONTENTS="$URL2
+test_url_and_file_listing() {
+  # Mixed URL and file listing downloading  
+  FILECONTENTS="$URL2
 $URL3"
-assert_equal "$(build $URL1 $URL2 $URL3)" "$($RS $URL1 <(echo "$FILECONTENTS"))"
+  assert_equal "$(build $URL1 $URL2 $URL3)" "$($RS $URL1 <(echo "$FILECONTENTS"))"
+}
+
+run "test_single_url"
+run "test_file_listing"
+run "test_url_and_file_listing"
