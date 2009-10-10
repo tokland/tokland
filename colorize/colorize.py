7b714cc6bb90575ea3986186ac291f525ec1f0e0
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2009-10-10 15:03:12 +0000

    colorize regular expressions

---
+++ colorize/colorize.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+"""
+Detect and colorize regular expressions in files or standard input.
+"""
+import sys
+import re
+
+ANSI_COLOR_TABLE = {	
+    "grey": 30,
+    "red": 31, 
+    "green": 32, 
+    "yellow": 33, 
+    "blue": 34, 
+    "magenta": 35, 
+    "cyan": 36, 
+    "white": 37,
+}
+
+def ansi_colorize(string, regexp, fg_color):
+    """Colorize all matches of regexp found in string s with
+    the ANSI string defined by a color string"""
+    code = ANSI_COLOR_TABLE[fg_color]
+    ansi_match = "\033[1;%dm%%s\033[0m" % code
+    def _replace(match):
+        """Surroung match with ANSI color code""" 
+        return ansi_match % match.group(1)
+    return re.sub('(%s)' % regexp, _replace, string)
+
+def _main(args):
+    """Main function for colorize."""
+    import optparse
+    usage = """usage: colorize [options] REGEXP [FILE]
+
+    Colorize matched regular expression in file or standard input
+    """ 
+    colors = ", ".join(ANSI_COLOR_TABLE.keys())
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-c', '--color', dest='color', default="red", 
+        metavar="STRING", type="string", help='Highlight color (%s)' % colors)
+    options, arguments = parser.parse_args(args)
+    if not arguments:
+        parser.print_help()
+        return 2
+    elif options.color not in ANSI_COLOR_TABLE:
+        parser.print_help()
+        sys.stderr.write("\nColor not valid: %s\n" % options.color)        
+        return 3
+    regexp, paths = arguments[0], arguments[1:]
+    streams = (map(open, paths) if paths else [sys.stdin])    
+    for stream in streams: 
+        for line in stream:
+            sys.stdout.write(ansi_colorize(line, regexp, options.color))
+
+
+if __name__ == "__main__":
+    sys.exit(_main(sys.argv[1:]))
