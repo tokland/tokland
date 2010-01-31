866c94aeddabebf0d81edbde0fa3cfae8af20e32
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-01-31 11:31:26 +0000

    support for programmes

--- bbc/download_episode.sh
+++ bbc/download_episode.sh
@@ -6,11 +6,13 @@
 # 
 #   $ bash download_episode.sh "http://www.bbc.co.uk/iplayer/episode/p005x3x5/World_Briefing_27_01_2010/"
 #
-# Author: Arnau Sanchez <tokland@gmail.com> (January 2010)
-#
+# Author: Arnau Sanchez <tokland@gmail.com> 
+# Date: January 2010
 # Website: http://code.google.com/p/tokland/wiki/Projects
 #
 set -e
+
+# Generic functions
 
 debug() { echo "$@" >&2; }
 
@@ -18,7 +20,11 @@ parse() { local S=$(sed -n "s/^.*$1.*$/\1/p") && test "$S" && echo "$S"; }
 
 parse_attr() { echo "$1" | parse "$2=\"\([^\"]*\)\""; }
 
+match() { grep -q "$1" <<< "$2"; }
+
 download() { curl -s "$@"; }
+
+# RTMP functions
 
 download_rtmp() {
   local HOST=$1
@@ -27,7 +33,10 @@ download_rtmp() {
   rtmpdump -r "rtmp://$HOST" -a "$APP" --playpath "$PLAYPATH" --flv -
 }
 
+# BBC functions
+
 download_episode() {
+  local URL=$1
   EPISODE=$(echo $URL | grep -o "\(episode\|console\)/[^/]*" | cut -d"/" -f2) 
   test "$EPISODE" || { debug "cannot parse episode: $URL"; return 1; }
   debug "episode: $EPISODE"
@@ -35,31 +44,48 @@ download_episode() {
   TITLE=$(echo "$EPISODE_XML" | grep -A1 "<item " | tail -n1 |  
     parse '<title>\(.*\)<\/title>') || return 1
   debug "title: $TITLE"
+  
   ITEM=$(echo "$EPISODE_XML" | grep "<item " | parse 'identifier="\([^"]*\)"') || return 1
   XML=$(download "http://www.bbc.co.uk/mediaselector/4/mtis/stream/$ITEM") || return 1
   CONNECTIONS=$(echo $XML | sed "s/>/>\n/g" | grep "<connection")
   OUTPUT=$(echo "$TITLE.flv" | sed "s/\//_/g")
-  if echo "$CONNECTIONS" | grep -q "wsondemandflash"; then
+  
+  if match "wsondemandflash" "$CONNECTIONS"; then
     CONNECTION=$(echo "$CONNECTIONS" | grep "wsondemandflash" | head -n1)
     SERVER=$(parse_attr "$CONNECTION" "server")
     IDENTIFIER=$(parse_attr "$CONNECTION" "identifier" | recode html..utf8)
     debug "rtmp-wsondemandfash: server=$SERVER, identifier=$IDENTIFIER"
     download_rtmp "$SERVER" "ondemand" "$IDENTIFIER" > "$OUTPUT" || return 1
-  elif echo "$CONNECTIONS" | grep -q 'bbcmedia.fcod.llnwd.net'; then
+  elif match "bbcmedia.fcod.llnwd.net" "$CONNECTIONS"; then
     CONNECTION=$(echo "$CONNECTIONS" | grep 'bbcmedia.fcod.llnwd.net' | head -n1)
     SERVER=$(parse_attr "$CONNECTION" "server")
     IDENTIFIER=$(parse_attr "$CONNECTION" "identifier")
     APP=$(parse_attr "$CONNECTION" "application")
     AUTH=$(parse_attr "$CONNECTION" "authString")
     PLAYPATH=$(echo "$IDENTIFIER?$AUTH" | recode html..utf8)
-    debug "rtmp-bbcmedia: server=$SERVER, identifier=$IDENTIFIER, auth=$AUTH"
+    debug "rtmp-bbcmedia: server=$SERVER, APP=$app, identifier=$IDENTIFIER, auth=$AUTH"
     download_rtmp "$SERVER" "$APP" "$PLAYPATH" > "$OUTPUT" || return 1
   fi
+  
   echo "$OUTPUT"
+}
+
+download_episodes() {
+  local URL=$1
+  if echo "$URL" | grep -q "www.bbc.co.uk/programmes"; then 
+    EPISODES=$(download "$URL" | grep 'title="Listen now')
+    parse_attr "$0EPISODES" "href" | while read EPISODE_URL; do
+      debug "episode URL: $EPISODE_URL"
+      download_episode "$EPISODE_URL"
+    done
+  else
+    download_episode "$URL"
+  fi
 }
 
 # Main
 
 URL=$1
-test "$URL" || { debug "Usage: $(basename $0) EPISODE_URL"; exit 1; }
-download_episode "$URL"
+test "$URL" || 
+  { debug "Usage: $(basename $0) CONSOLE_URL|EPISODE_URL|PROGRAMME_URL"; exit 1; }
+download_episodes "$URL"
