b54399632490cb897a47c4b3b5a4f296d788fac3
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-02-14 10:34:37 +0000

    convert lavanguardia chess pdf to FEN

---
+++ lavanguardia/chess.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import operator
+import itertools
+
+WHITE_PIECES = "RKBPQN"
+BLACK_PIECES = "rkbpqn"
+
+def get_san_rank(rank):
+	if not rank:
+		return ""
+	nblank = len(list(itertools.takewhile(operator.not_, rank)))
+	if nblank:
+		return str(nblank) + get_san_rank(rank[nblank:]) 
+	else: 
+	  return rank[0] + get_san_rank(rank[1:])
+
+def get_fen_notation(board, whitetomove=True, whitecastle="kq",
+		blackcastle="kq", enpassant=None, halfmove=0, fullmove=1):
+	adj = lambda s: s or "-"
+	pieces = "/".join(get_san_rank(rank) for rank in board)
+	white_to_move_string = ("w" if whitetomove else "b")
+	castle = whitecastle.upper() + blackcastle.lower()
+	return " ".join([
+	  pieces, 
+	  white_to_move_string, 
+	  adj(castle), 
+	  adj(enpassant), 
+	  str(halfmove), 
+	  str(fullmove),
+	 ])
---
+++ lavanguardia/pdf2fen.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+import sys
+import re
+import subprocess
+
+# Package modules
+import chess
+
+# Generic functions
+
+def debug(line):
+    sys.stderr.write(line + "\n")
+    sys.stderr.flush()
+  
+def run(command, inputdata=None, **kwargs):
+    default_kwargs = dict(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    popen = subprocess.Popen(command, **dict(default_kwargs, **kwargs))
+    outdata, errdata = popen.communicate(inputdata)
+    return popen.returncode, outdata, errdata
+
+def first(it, pred=bool):
+    for x in it:
+        if pred(x):
+            return x
+  
+def create_matrix(shape, value=None):
+    if not shape:
+        return value
+    return [create_matrix(shape[1:], value) for _ in xrange(shape[0])]
+
+# Specific functions
+  
+def debug_board(board):
+    for index, line in enumerate(board):
+        debug("%s %s" % ((8 - index), " ".join((c or "-") for c in line)))
+    debug(" "*2 + " ".join("abcdefgh")) 
+
+def parse_text(line):
+  match = re.match("""
+    <text\s*
+      top="(?P<top>\d+)"\s* 
+      left="(?P<left>\d+)"\s* 
+      width="(?P<width>\d+)"\s* 
+      height="(?P<height>\d+)"\s* 
+      font="(?P<font>\d+)">\s*
+    (?P<text>.*)</text>""", line, re.VERBOSE)
+  return match.groupdict()
+
+def create_board(board_lines, ref_line):
+  spaces = [u' ', u'\uf02c']
+  equivalences = {
+    u'\uf06a': 'r', u'\uf064': 'r',
+    u'\uf067': 'p', u'\uf061': 'p',
+    u'\uf06c': 'k',
+    u'\uf062': 'n', u'\uf068': 'n',
+    u'\uf065': 'q',
+    u'\uf063': 'b', u'\uf069': 'b',
+    
+    u'\uf045': 'Q',
+    u'\uf046': 'K', u'\uf04c': 'K',  
+    u'\uf041': 'P', u'\uf047': 'P',
+    u'\uf04a': 'R', u'\uf044': 'R',
+    u'\uf048': 'N', u'\uf042': 'N',
+    u'\uf043': 'B', u'\uf049': 'B',
+  }
+  y_offset = 573 - 575
+  x_offset = 641 - 628     
+
+  ref_info = parse_text(ref_line)
+  ref_top, ref_left = map(int, [ref_info["top"], ref_info["left"]])  
+  board = create_matrix((8, 8), None)
+    
+  for y, board_line in enumerate(board_lines):
+    info = parse_text(board_line)
+    top, left = map(int, [info["top"], info["left"]])
+    #y = (top - ref_top - y_offset)/16
+    x = int(round((left - ref_left - x_offset)/16.0))
+    for xindex, char in enumerate(unicode(info["text"], "utf8")):
+      if char not in spaces:
+        assert (char in equivalences), "Piece not configured: %s (%s%d)" % \
+          (repr(char), "abcdefgh"[x+xindex], 8-y)
+        board[y][x+xindex] = equivalences[char]
+  return board
+    
+def pdf2fen(pdffile):
+  debug("pdf file: %s" % pdffile)
+  retcode, output, _ = run(["pdftohtml", "-xml", "-stdout", pdffile])
+  assert (retcode == 0), "Error running pdftohtml"
+  lines = output.splitlines()
+  
+  tomove = first(re.search(r'font="5"><b>(\w+) JUEGAN Y', line) 
+    for line in lines).group(1).lower()
+  assert (tomove in ["blancas", "negras"]), "Error parsing who is to move"
+  debug("to move: %s" % tomove)
+  white_to_move = (tomove == "blancas")
+  
+  ref_line = first(line for line in lines if 'font="8"' in line)
+  assert ref_line, "Error finding reference line"  
+  board_lines = [line for line in lines if 'font="9"' in line]
+  assert (len(board_lines) == 8), "Wrong board lines: %d" % len(board_lines)
+  board = create_board(board_lines, ref_line)
+  debug_board(board) 
+  return chess.get_fen_notation(board, whitetomove=white_to_move, 
+    whitecastle="kq", blackcastle="kq")
+  
+# Main
+
+def main(args):
+  pdffile, = args  
+  print pdf2fen(pdffile)
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
