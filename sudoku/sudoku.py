de34ed1aa4bdd9e8446bf7cb8b2246b0d70f37aa
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-04-11 22:22:58 +0000

    refactor

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -22,39 +22,39 @@ Example:
 import re
 import sys
 
-def copy_board(board, new_values):
-    """Return a copy of board setting values in new_values dictionary."""
-    return [[new_values.get((r, c), board[r][c]) for c in range(9)] for r in range(9)] 
+def copy_board(board, values):
+    """Return a copy of board setting values in values dictionary."""
+    return [[values.get((r, c), board[r][c]) for c in range(9)] for r in range(9)] 
             
 def get_alternatives_for_square(board, nrow, ncolumn):
     """Return sequence of valid digits for (nrow, ncolumn) square in board."""
-    box_row, box_column = 3 * (nrow / 3), 3 * (ncolumn / 3)
-    nums_in_box = [board[r][c] for r in range(box_row, box_row+3) 
-                               for c in range(box_column, box_column+3)]
+    def box(x, n=3):
+        start = (x / n) * n
+        return range(start, start + n)
+    nums_in_box = [board[r][c] for r in box(nrow) for c in box(ncolumn)]
     nums_in_row = [board[nrow][c] for c in range(9)]
     nums_in_column = [board[r][ncolumn] for r in range(9)]
     groups = [filter(bool, x) for x in [nums_in_box, nums_in_row, nums_in_column]]
     return sorted(set(range(1, 10)) - set(sum(groups, []))) 
      
 def solve(board):
-    """Return a solved Sudoku board (return None if if no valid solution)."""
-    for nrow in range(9):
-        for ncolumn in range(9):
-            if board[nrow][ncolumn]:
-                # the square has a digit, go to the next one
-                continue
-            for test_digit in get_alternatives_for_square(board, nrow, ncolumn):
-                new_board = copy_board(board, {(nrow, ncolumn): test_digit})
-                solved_board = solve(new_board)
-                if solved_board:
-                    # return the solved board all the way up to break recursion
-                    return solved_board
-            return
-    # all squares are filled, so this must be the solution. 
+    """Return a solved Sudoku board (None if it has no solution)."""
+    for nrow, ncolumn in ((r, c) for r in range(9) for c in range(9)):
+        if board[nrow][ncolumn]:
+            # skip square with a digit already set 
+            continue
+        for test_digit in get_alternatives_for_square(board, nrow, ncolumn):
+            test_board = copy_board(board, {(nrow, ncolumn): test_digit})
+            solved_board = solve(test_board)
+            if solved_board:
+                # return the solved board all the way up to break recursion
+                return solved_board
+        return
+    # all squares are filled so this must be the first solution. 
     return board 
 
 def lines2board(lines):
-    """Return a board using 0 for empty squares and removing all spaces."""
+    """Return a board using 0's for empty squares ignoring all spaces."""
     return [[(int(c) if c in "123456789" else 0) for c in re.sub("\s+", "", line)] 
             for line in lines if line.strip()]
 
