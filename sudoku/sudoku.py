4d73e42e865b29ec2966605336836b32d5ce5095
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-04-12 20:58:48 +0000

    minor refactor

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -9,8 +9,8 @@ Webpage: http://code.google.com/p/wiki/Projects
 = How it works:
     
 The algorithm starts by finding the first empty square on the board and getting
-the list of valid digits for this square. Then it loops for every possible 
-value calling recursively to the solver function. 
+the list of valid digits for this square. Then it tries these digits calling 
+the solver function.
 
 If at some point no valid digit for a square if found (meaning a previously 
 tested digit was wrong) the algorithm goes back. Eventually, all squares 
@@ -19,21 +19,21 @@ up to the caller.
   
 = How to run the script
        
-Create a text-file with a 9x9 grid, using spaces the way you prefer. Example:
+Create a text-file with a 9x9 grid, using spaces the way you like. For example:
   
-    6-- --- -83
-    --7 1-- --4
-    --9 --2 7--
+    6 - -  - - -  - 8 3
+    - - 7  1 - -  - - 4
+    - - 9  - - 2  7 - -
 
-    --- 5-9 ---
-    1-- 348 --9
-    --- 7-1 ---
+    - - -  5 - 9  - - -
+    1 - -  3 4 8  - - 9
+    - - -  7 - 1  - - -
 
-    --5 9-- 3--
-    3-- --6 1--
-    76- --- --8
+    - - 5  9 - -  3 - -
+    3 - -  - - 6  1 - -
+    7 6 -  - - -  - - 8
   
-And then run the script with the text file as argument:
+And then run the script with the text filename as argument:
 
 $ python sudoku.py mysoduku.txt
 """
@@ -41,7 +41,7 @@ import re
 import sys
 
 def first(it, default=None):
-    """Return first element in iterator (None if exhausted)."""
+    """Return first element in iterator (default if exhausted)."""
     return next(it, default)
 
 def copy_board(board, sets):
@@ -64,13 +64,12 @@ def solve(board):
     """Return a solved Sudoku board (None if no solution was found)."""
     pos = first((r, c) for r in range(9) for c in range(9) if not board[r][c])
     if not pos:
-        # all squares are filled, so this board is the solution
-        return board
+        return board # all squares are filled, so this board is the solution
     nrow, ncolumn = pos
     for test_digit in get_alternatives_for_square(board, nrow, ncolumn):
         test_board = copy_board(board, {(nrow, ncolumn): test_digit})
         solved_board = solve(test_board)
-        if solved_board:              
+        if solved_board:
             return solved_board
 
 def lines2board(lines):
