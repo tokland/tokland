ef8de6864b496877205fe72b4c5501b5c54a9c3e
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-04-12 18:23:41 +0000

    refactor solve()

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,65 +1,77 @@
 #!/usr/bin/python
 """
-Simple brute-force Sudoku solver. 
+Brute-force Sudoku solver written in functional-programming style.
 
-- Functional-programming style.
-- Recursive backtracking.
-- No heuristics, no optimization at all.
- 
-The board must be a 9x9 grid (use any non-digit char to denote empty squares).
-You can format the board with spaces the way you like (they will be removed).
+Author: Arnau Sanchez <tokland@gmail.com>
+License: GNU GPL v3.0
+Webpage: http://code.google.com/p/wiki/Projects 
 
-Example (mysudoku.txt):
+= How it works:
+    
+The algorithm starts by finding the first empty square on the board and getting
+the list of valid digits for this square. Then it loops for every possible 
+value calling recursively to the solver function. 
+
+If at some point no valid digit for a square if found (meaning a previously 
+tested digit was wrong) the algorithm goes back. Eventually, all squares 
+will be filled and the board containing the solution will travel all the stack 
+up to the caller.
   
-  6-- --- -83
-  --7 1-- --4
-  --9 --2 7--
-
-  --- 5-9 ---
-  1-- 348 --9
-  --- 7-1 ---
-
-  --5 9-- 3--
-  3-- --6 1--
-  76- --- --8
+= How to run the script
+       
+Create a text-file with a 9x9 grid, using spaces the way you prefer. Example:
   
+    6-- --- -83
+    --7 1-- --4
+    --9 --2 7--
+
+    --- 5-9 ---
+    1-- 348 --9
+    --- 7-1 ---
+
+    --5 9-- 3--
+    3-- --6 1--
+    76- --- --8
+  
+And then run the script with the text file as argument:
+
 $ python sudoku.py mysoduku.txt
 """
 import re
 import sys
 
+def first(it, default=None):
+    """Return first element in iterator (None if exhausted)."""
+    return next(it, default)
+
 def copy_board(board, sets):
-    """Return a copy of board setting new squares from values dictionary."""
+    """Return a copy of board setting new squares from 'values' dictionary."""
     return [[sets.get((r, c), board[r][c]) for c in range(9)] for r in range(9)]
             
 def get_alternatives_for_square(board, nrow, ncolumn):
     """Return sequence of valid digits for square (nrow, ncolumn) in board."""
-    def _box(pos, size=3):
-        """Return indexes to cover a box (sub-matrix of board) in one axis."""
-        start = (pos // size) * size
+    def _box(idx, size=3):
+        """Return indexes to cover a box (3x3 sub-matrix of a board)."""
+        start = (idx // size) * size
         return range(start, start + size)
     nums_in_box = [board[r][c] for r in _box(nrow) for c in _box(ncolumn)]
     nums_in_row = [board[nrow][c] for c in range(9)]
     nums_in_column = [board[r][ncolumn] for r in range(9)]
     groups = [nums_in_box, nums_in_row, nums_in_column]
-    return sorted(set(range(1, 10)) - reduce(set.union, map(set, groups))) 
+    return sorted(set(range(1, 9+1)) - reduce(set.union, map(set, groups))) 
      
 def solve(board):
-    """Return a solved Sudoku board (None if it has no solution)."""
-    for nrow, ncolumn in ((r, c) for r in range(9) for c in range(9)):
-        if board[nrow][ncolumn]:
-            # digit set, move to the next square
-            continue 
-        for test_digit in get_alternatives_for_square(board, nrow, ncolumn):
-            test_board = copy_board(board, {(nrow, ncolumn): test_digit})
-            solved_board = solve(test_board)
-            if solved_board:              
-                # return the solved board all the way up to break recursion
-                return solved_board
-        # no solution was found for this square, so let's go back to keep trying
-        return
-    # all squares are filled so this must be the solution. 
-    return board 
+    """Return a solved Sudoku board (None if no solution was found)."""
+    pos = first((r, c) for r in range(9) for c in range(9) if not board[r][c])
+    if not pos:
+        # all squares are filled, so this board is the solution
+        return board
+    nrow, ncolumn = pos
+    for test_digit in get_alternatives_for_square(board, nrow, ncolumn):
+        test_board = copy_board(board, {(nrow, ncolumn): test_digit})
+        solved_board = solve(test_board)
+        if solved_board:              
+            return solved_board
 
 def lines2board(lines):
     """Parse a text board setting 0's for empty squares."""
@@ -68,7 +80,7 @@ def lines2board(lines):
             for line in lines if line.strip()]
 
 def main(args):
-    """Solve a Sudoku board read from a file."""
+    """Solve a Sudoku board read from a text file."""
     from pprint import pprint
     path, = args
     board = lines2board(open(path))
