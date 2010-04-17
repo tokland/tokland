4c29858c3cefba1accaf8b53efc486f2e8f242a0
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-04-17 14:59:49 +0000

    sudoku2 with dict solution

--- sudoku/sudoku2.py
+++ sudoku/sudoku2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-Brute-force Sudoku solver written in functional-programming style.
+Brute-force Sudoku solver written in functional-programming style (dictionary version).
 
 Author: Arnau Sanchez <tokland@gmail.com>
 License: GNU GPL v3.0
@@ -8,7 +8,7 @@ Webpage: http://code.google.com/p/wiki/Projects
 
 = How it works:
     
-The algorithm starts by finding the first empty square on the board and getting
+The algorithm starts by finding an empty square on the board and getting
 the list of valid digits for this square. Then it tries these digits calling 
 the solver function.
 
@@ -35,56 +35,57 @@ Create a text-file with a 9x9 grid, using spaces the way you like. For example:
   
 And then run the script with the text filename as argument:
 
-$ python sudoku.py mysoduku.txt
+$ python sudoku2.py mysoduku.txt
 """
 import re
 import sys
 
-def first(it, default=None):
-    """Return first element in iterator (default if exhausted)."""
-    return next(it, default)
-
-def copy_board(board, sets):
-    """Return a copy of board setting new squares from 'sets' dictionary."""
-    return [[sets.get((r, c), board[r][c]) for c in range(9)] for r in range(9)]
-            
 def get_alternatives_for_square(board, nrow, ncolumn):
     """Return sequence of valid digits for square (nrow, ncolumn) in board."""
-    def _box(idx, size=3):
+    def _box(idx):
         """Return indexes to cover a box (3x3 sub-matrix of a board)."""
-        start = (idx // size) * size
-        return range(start, start + size)
-    nums_in_box = [board[r][c] for r in _box(nrow) for c in _box(ncolumn)]
-    nums_in_row = [board[nrow][c] for c in range(9)]
-    nums_in_column = [board[r][ncolumn] for r in range(9)]
+        start = (idx // 3) * 3 
+        return range(start, start + 3)
+    nums_in_box = [board.get((r, c), None) for r in _box(nrow) for c in _box(ncolumn)]
+    nums_in_row = [board.get((nrow, c), None) for c in range(9)]
+    nums_in_column = [board.get((r, ncolumn), None) for r in range(9)]
     nums = nums_in_box + nums_in_row + nums_in_column
     return sorted(set(range(1, 9+1)) - set(nums)) 
+
+ranges = [(x, y) for x in range(9) for y in range(9)]
      
 def solve(board):
     """Return a solved Sudoku board (None if no solution was found)."""
-    pos = first((r, c) for r in range(9) for c in range(9) if not board[r][c])
-    if not pos:
-        return board # all squares are filled, so this board is the solution
-    nrow, ncolumn = pos
+    if len(board) == 9 * 9:
+        return board
+    nrow, ncolumn = ((x, y) for (x, y) in ranges if (x, y) not in board).next()
     for test_digit in get_alternatives_for_square(board, nrow, ncolumn):
-        test_board = copy_board(board, {(nrow, ncolumn): test_digit})
+        test_board = dict(board, **{(nrow, ncolumn): test_digit})
         solved_board = solve(test_board)
         if solved_board:
             return solved_board
 
+def print_board(board, stream=sys.stdout):
+    """Print a Sudoku board to stream."""
+    lines = [[str(board.get((r, c), "-")) for c in range(9)] for r in range(9)]
+    stream.writelines(" ".join(line)+"\n" for line in lines)
+    
 def lines2board(lines):
     """Parse a text board stripping spaces and setting 0's for empty squares."""
-    spaces = re.compile("\s+")
-    return [[(int(c) if c in "123456789" else 0) for c in spaces.sub("", line)]
-            for line in lines if line.strip()]
+    def _get_squares(lines):
+        for nrow, line in enumerate(line for line in lines if line.strip()):
+            for ncolumn, c in enumerate(re.sub("\s+", "", line)):
+                if c in "1234546789":
+                    yield ((nrow, ncolumn), int(c))                    
+    return dict(_get_squares(lines))
 
 def main(args):
     """Solve a Sudoku board read from a text file."""
-    from pprint import pprint
     path, = args
     board = lines2board(open(path))
-    pprint(board)
-    pprint(solve(board))
+    print_board(board)
+    print "-" * (2*9-1)
+    print_board(solve(board))
     
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
