5dd1b877f755388c9a0405baee46e2d65d32c80a
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-04-25 20:36:31 +0000

    minor refactor

--- tools/loop.sh
+++ tools/loop.sh
@@ -11,15 +11,20 @@
 #
 # $ loop -b "{1..3} 10" -m 5 -t 2m myapp arg1 arg2
 # 
-# Note that you can use bash {START..END} notation for return values.
+# See that you can use bash {START..END} notation for return values (it will be
+# expanded inside the script, so you must quote them)
 #
 set -e
 
-debug() { test "$QUIET" -ne 1 && echo "--- $@" >&2; }
+stderr() { test "$QUIET" -ne 1 && echo "$@" >&2; }
+
+debug() { test "$QUIET" -ne 1 && stderr "--- $@"; }
 
 infinite_seq() { yes "" | sed -n "="; }
 
 word_in_list() { grep -qw "$2" <<< "$1"; }
+
+tobool() { "$@" > /dev/null && echo 1 || echo 0; }
 
 loop() {
   local MAXTRIES=$1; local LOOPWAIT=$2; local TIMEOUT=$3; 
@@ -28,9 +33,11 @@ loop() {
   
   BREAK_RETVALS=$(eval echo $BREAK_RETVALS)
   ITIME=$(date +%s)
+  exec 3<&0
+   
   { test "$MAXTRIES" && seq $MAXTRIES || infinite_seq; } | while read TRY; do
-    "$@" && RETVAL=0 || RETVAL=$?
-    word_in_list "$BREAK_RETVALS" $RETVAL && INARRAY=1 || INARRAY=0 
+    "$@" <&3 && RETVAL=0 || RETVAL=$?
+    INARRAY=$(tobool word_in_list "$BREAK_RETVALS" $RETVAL)
     debug "try=$TRY, retval: $RETVAL (break retvals: $BREAK_RETVALS, negate: $NEGATE)"
     test \( $INARRAY -eq 1 -a "$NEGATE" -eq 0 \) -o \
          \( $INARRAY -eq 0 -a "$NEGATE" -eq 1 \) && return 0
@@ -40,6 +47,8 @@ loop() {
     } 
 	  sleep $LOOPWAIT
   done
+  
+  debug "max retries reached: $MAXTRIES"  
   return 1
 }  
 
@@ -49,16 +58,17 @@ QUIET=0
 NEGATE=0
 TIMEOUT=
 BREAK_RETVALS=
+test $# -eq 0 && set -- "-h"
 while getopts "t:m:w:b:nqh" ARG; do
   case "$ARG" in
   q) QUIET=1;;
-	m) MAXTRIES=$OPTARG;;
-	w) LOOPWAIT=$OPTARG;;
-	t) TIMEOUT=$OPTARG;;
-	b) BREAK_RETVALS=$OPTARG;;
-	n) NEGATE=1;;
-	h) debug "Usage: $(basename $0) [-q] [-n] [-b BREAK_RETVALS] [-m MAXTRIES] \ 
-	        [-w LOOPWAIT] [-t TIMEOUT] COMMAND [ARGS]"
+  m) MAXTRIES=$OPTARG;;
+  w) LOOPWAIT=$OPTARG;;
+  t) TIMEOUT=$OPTARG;;
+  b) BREAK_RETVALS=$OPTARG;;
+  n) NEGATE=1;;
+  h) stderr "Usage: $(basename $0) [-q] [-n] [-b BREAK_RETVALS (default: 0)] [-m MAXTRIES]  
+               [-w LOOPWAIT] [-t TIMEOUT] COMMAND [ARGS]"
 		 exit 1;;
 	esac
 done
