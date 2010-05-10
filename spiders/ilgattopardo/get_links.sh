f490fef953a551fc1abbe45652363a2cb3946570
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-05-10 08:08:43 +0000

    minor

--- spiders/ilgattopardo/get_links.sh
+++ spiders/ilgattopardo/get_links.sh
@@ -4,7 +4,7 @@
 set -e
 
 URL="http://www.radio.rai.it/radio3/terzo_anello/alta_voce/archivio_2004/eventi/2004_02_03_gattopardo/##"
-curl "$URL" | grep -o "http://.*ram'" | tr -d "'" | while read URL; do
-  curl $URL
+curl "$RAM_URL" | grep -o "http://.*ram'" | tr -d "'" | while read URL; do
+  curl "$RAM_URL"
   echo # so we have one link per line
-done 
+done
