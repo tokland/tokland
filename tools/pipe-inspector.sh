4898143c5f8a5e3a3c499bfb569d60d4ccf6e35e
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-06-08 15:01:58 +0000

    minor

--- tools/pipe-inspector.sh
+++ tools/pipe-inspector.sh
@@ -15,16 +15,17 @@ SPEED_CHARS=
 NCOLS=${COLUMNS:-80}
 ITIME=$(date +%s)
 LAST_SPEED_TIME=0
+
 while read LINE; do
   NCHARS=$(($NCHARS + ${#LINE}))
   ELAPSED=$(($(date +%s) - $ITIME))
   NLINES=$(($NLINES + 1))
   if test $ELAPSED -ne $LAST_SPEED_TIME; then
-    LAST_SPEED_TIME=$ELAPSED      
     SPEED_LINES=$(($NLINES / $ELAPSED))
     SPEED_CHARS=$(($NCHARS / $ELAPSED))
+    LAST_SPEED_TIME=$ELAPSED      
   fi       
-  LOG="$NCHARS ($SPEED_CHARS chars/sec) - $NLINES ($SPEED_LINES lines/sec)"
+  LOG="$NCHARS (${SPEED_CHARS:--} chars/sec) - $NLINES (${SPEED_LINES:--} lines/sec)"
   debug -n -e "\r$LOG$(repeat_string ${#LOG} '\b')"
   echo $LINE
 done
