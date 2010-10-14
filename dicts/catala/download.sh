0a3ba1be1a46b367c1ffcaefb0aec86f69ade88c
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-10-14 09:18:02 +0000

    update catala dict crawler

---
+++ dicts/catala/download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+debug() { 
+  echo "$@" >&2; 
+}
+
+OUTPUTDIR="html/"
+mkdir -p "$OUTPUTDIR"
+START_URL="http://www.diccionari.cat/cgi-bin/AppDLC3.exe?APP=CERCADLC&GECART=0"
+URL=$START_URL
+while true; do
+  debug "page: $URL"
+  PAGE=$(curl "$URL")
+  echo "$PAGE" | sed "s#\(</[^>]*>\)#\1\n#g" | grep 'LLISTA_D' | 
+      sed -n 's/^.*href="\(.*\)".*CLASS.*>\(.*\)<.*$/\1 \2/p' | while read URL WORD; do
+    debug "$WORD: $URL"
+    FILE="$OUTPUTDIR/$WORD.html"
+    test -e "$FILE" -a -s "$FILE" && 
+      { debug "skip existing file: $FILE"; continue; }
+    curl -o "$FILE" "$URL"
+    echo $FILE
+  done
+  P=$(echo "$PAGE" | grep -o 'javascript:seguents([^;]*' | cut -d"(" -f2 | tr -d ')') || true
+  test "$P" || break
+  URL="http://www.diccionari.cat/cgi-bin/AppDLC3.exe?APP=SEGUENTS&P=$P"
+done
