f37ab0bb3aa0490f4691259339483410a150cea0
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-10-24 17:13:10 +0000

    [loop] minor refactor

--- tools/loop.sh
+++ tools/loop.sh
@@ -6,15 +6,14 @@
 #
 # $ loop myapp arg1 arg2
 #
-# - Complete example: Run 'myapp arg1 arg2' until the return value is NOT one 
-#   of 1, 2, 3 or 10. Loop over the command at most 5 times and never for 
-#   more than 2 minutes.
+# - Complex example: Run 'myapp arg1 arg2' until the return value is NOT  
+#   1, 2, 3 or 10. In addition, loop over the command at most 5 times and never 
+#   not for more than 2 minutes.
 #
 # $ loop -n -b "{1..3} 10" -m 5 -t 2m myapp arg1 arg2
 # 
-# See how you can use bash intervals notation ({START..END}) for return 
-# values (this will be expanded in the script, so quote them)
-
+# Note that you can use the Bash intervals syntax {START..END} for statuses. 
+#
 set -e
 
 # Global variables
@@ -29,13 +28,13 @@ stderr() { echo "$@" >&2; }
 # Write to standard error unless QUIET is enabled (1)
 debug() { test "$QUIET" -ne 1 && stderr "--- $@" || true; }
 
-# Return an infinite sequence (starts at 1). For-loop are boring, let's use pipes. 
+# Return an infinite sequence (starts at 1). For-loops are boring. 
 infinite_seq() { yes "" | sed -n "="; }
 
-# Succeed if string $2 is inside $1
+# Succeed if regexp $2 is found in $1
 word_in_list() { grep -qw "$2" <<< "$1"; }
 
-# Run wrapped command ($@) and return 1 if ok, 0 if failed
+# Run command ($@) and return 1 if ok, 0 if failed
 tobool() { "$@" > /dev/null && echo 1 || echo 0; }
 
 ###
@@ -47,7 +46,7 @@ tobool() { "$@" > /dev/null && echo 1 || echo 0; }
 #   4: Max retries reached
 loop() {
   local MAXTRIES=$1; local LOOPWAIT=$2; local TIMEOUT=$3; 
-  local BREAK_RETVALS=${4:-0}; local NEGATE=${5:-0}; local FOREVER=$6
+  local BREAK_RETVALS=${4:-0}; local COMPLEMENT=${5:-0}; local FOREVER=$6
   shift 6
   
   BREAK_RETVALS=$(eval echo $BREAK_RETVALS)
@@ -56,15 +55,16 @@ loop() {
    
   while read TRY; do
     "$@" <&3 && RETVAL=0 || RETVAL=$?
-    debug "try=$TRY, retval: $RETVAL (break retvals: $BREAK_RETVALS, negate: $NEGATE, forever: $FOREVER)"
+    debug "try=$TRY, retval: $RETVAL (break retvals: $BREAK_RETVALS, complement: $COMPLEMENT, forever: $FOREVER)"
     test "$FOREVER" = 1 && continue
     INARRAY=$(tobool word_in_list "$BREAK_RETVALS" $RETVAL)
-    test \( $INARRAY -eq 1 -a "$NEGATE" -eq 0 \) -o \
-         \( $INARRAY -eq 0 -a "$NEGATE" -eq 1 \) && return 0
-    test "$TIMEOUT" && expr $(date +%s) - $ITIME + $LOOPWAIT \> $TIMEOUT >/dev/null && {
+    if test \( $INARRAY -eq 1 -a "$COMPLEMENT" -eq 0 \) -o \
+         \( $INARRAY -eq 0 -a "$COMPLEMENT" -eq 1 \); then
+      return 0
+    elif test "$TIMEOUT" -a $(($(date +%s) - $ITIME + $LOOPWAIT)) -gt $TIMEOUT; then
       debug "timeout reached: $TIMEOUT"
       return 3
-    } 
+    fi
     sleep $LOOPWAIT
   done < <(test "$MAXTRIES" && seq $MAXTRIES || infinite_seq)
   debug "max retries reached: $MAXTRIES"  
@@ -72,34 +72,35 @@ loop() {
 }  
 
 usage() {
-  stderr -e "Usage: $(basename $0) [OPTIONS] COMMAND [COMMAND_ARGS]\n"
+  stderr -e "Usage: $(basename $0) [OPTIONS] COMMAND [ARGS ...]\n"
+  stderr -e "Run command until the command is successful.\n"
   stderr "Options:"
-  stderr "  -q: Be quiet"
-  stderr "  -n: Negate the break statuses (see -b)"
-  stderr "  -f: Force a never-ending loop"
-  stderr "  -b STRING: Space-separated list of status that break loop (defaults to '0')"
-  stderr "  -t SECONDS: maximum execution time before abortin the loop"
-  stderr "  -m MAXTRIES: Maximum tries before aborting the loop"
-  stderr "  -w SECONDS: time to wait between loop executions"
+  stderr "  -q:          Be quiet"
+  stderr "  -c:          Complement the break statuses (see -b)."
+  stderr "  -f:          Force a never-ending loop."
+  stderr "  -b STRING:   Space-separated list of statuses that break the loop."
+  stderr "  -t SECONDS:  Maximum execution time before aborting the loop."
+  stderr "  -m MAXTRIES: Maximum tries before aborting the loop."
+  stderr "  -w SECONDS:  Time to wait between loop executions."
 }
 
 ### Main
 
 MAXTRIES=
-LOOPWAIT=1 
-NEGATE=0
+LOOPWAIT=0 
+COMPLEMENT=0
 FOREVER=0
 TIMEOUT=
 BREAK_RETVALS=
 test $# -eq 0 && set -- "-h"
-while getopts "t:m:w:b:nqfh" ARG; do
+while getopts "t:m:w:b:cqfh" ARG; do
   case "$ARG" in
   q) QUIET=1;;
   m) MAXTRIES=$OPTARG;;
   w) LOOPWAIT=$OPTARG;;
   t) TIMEOUT=$OPTARG;;
   b) BREAK_RETVALS=$OPTARG;;
-  n) NEGATE=1;;
+  n) COMPLEMENT=1;;
   f) FOREVER=1;;
   h) usage; exit 2;;
 	*) usage; exit 2;;
@@ -107,4 +108,4 @@ while getopts "t:m:w:b:nqfh" ARG; do
 done
 shift $(($OPTIND-1))
 
-loop "$MAXTRIES" "$LOOPWAIT" "$TIMEOUT" "$BREAK_RETVALS" "$NEGATE" "$FOREVER" "$@"
+loop "$MAXTRIES" "$LOOPWAIT" "$TIMEOUT" "$BREAK_RETVALS" "$COMPLEMENT" "$FOREVER" "$@"
