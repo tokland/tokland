2074db58c5c895533cd9a29b8bc3e8105d7051bd
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-11-22 16:20:57 +0000

    toggle-panel minor

--- xfce/toggle-panel.sh
+++ xfce/toggle-panel.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+#
 # Toggle xfce4-panel visibility
+#
 set -e
 
 INFO=$(xwininfo -name xfce4-panel)
-STATE=$(echo "$INFO" | grep "Map State:" | head -n1 | awk -F: '{print $2}' | xargs)
-WID=$(echo "$INFO" | grep "Window id:" | head -n1 | awk -F: '{print $3}' | awk '{print $1}')
+WID=$(echo "$INFO" | grep -m1 "Window id:" | awk -F: '{print $3}' | awk '{print $1}')
+STATE=$(echo "$INFO" | grep -m1 "Map State:" | awk -F: '{print $2}' | xargs)
+
 if test "$STATE" = "IsViewable"; then
   xdotool windowminimize "$WID"
 else
