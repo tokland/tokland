996bf511093bc26f1b38824d0909b700b89465dc
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2011-05-14 14:40:31 +0000

    [archlinux-bootstrap] aesthetic changes

--- archlinux/arch-bootstrap.sh
+++ archlinux/arch-bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+
 # arch-bootstrap: Bootstrap a base Arch Linux system.
 #
 # Depends: coreutils, wget, sed, awk, tar, gzip, chroot, xz
@@ -8,19 +8,15 @@
 #
 # Some examples:
 #
-# $ bash arch-bootstrap.sh myarch 
-# $ bash arch-bootstrap.sh myarch x86_64 
-# $ bash arch-bootstrap.sh myarch x86_64 "ftp://ftp.archlinux.org"
-# 
-# Packages needed by pacman (BASIC_PACKAGES) can be obtained this way:
-# 
-# $ for PACKAGE in $(ldd /usr/bin/pacman | grep "=> /" | awk '{print $3}'); do 
-#     pacman -Qo $PACKAGE 
-#   done | awk '{print $5}' | sort -u | xargs
+#   $ bash arch-bootstrap.sh myarch 
+#   $ bash arch-bootstrap.sh myarch x86_64 
+#   $ bash arch-bootstrap.sh myarch x86_64 "ftp://ftp.archlinux.org"
 #
-set -e
+# And then chroot to the bootstrapped distro (administrator: root/root):
+#
+#   $ sudo chroot myarch
 
-### Generic functions
+set -e
 
 # Output to standard error
 stderr() { echo "$@" >&2; }
@@ -36,6 +32,11 @@ fetch() { wget -c --passive-ftp --quiet "$@"; }
 
 ### Main
 
+# Packages needed by pacman (BASIC_PACKAGES) has been obtained this way:
+# 
+#   $ for PACKAGE in $(ldd /usr/bin/pacman | grep "=> /" | awk '{print $3}'); do 
+#       pacman -Qo $PACKAGE 
+#     done | awk '{print $5}' | sort -u | xargs
 BASIC_PACKAGES=(acl attr bzip2 expat glibc libarchive libfetch openssl pacman 
                 pacman-mirrorlist xz zlib)
 EXTRA_PACKAGES=(filesystem coreutils bash grep awk file tar)
@@ -51,7 +52,7 @@ configure_pacman() {
 minimal_configuration() {
   local DEST=$1
   echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd"
-  # user root, password root
+  # create root user (password root)
   echo "root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi/:14657::::::" > "$DEST/etc/shadow"
   touch "$DEST/etc/group"
   echo "bootstrap" > "$DEST/etc/hostname"
@@ -60,7 +61,6 @@ minimal_configuration() {
 
 check_compressed_integrity() {
   local FILEPATH=$1
-  
   case "$FILEPATH" in
   *.gz) gunzip -t "$FILEPATH";;
   *.xz) xz -t "$FILEPATH";;
@@ -71,7 +71,6 @@ check_compressed_integrity() {
 
 uncompress() {
   local FILEPATH=$1; local DEST=$2
-  
   case "$FILEPATH" in
   *.gz) tar xzf "$FILEPATH" -C "$DEST";;
   *.xz) xz -dc "$FILEPATH" | tar x -C "$DEST";;
@@ -82,10 +81,10 @@ uncompress() {
 
 ### Main
 
-test $# -ge 1 || { 
-  stderr "Usage: $(basename "$0") DEST_DIR [i686|x86_64] [REPO_URL]"
+if test $# -lt 1; then
+  stderr "Usage: $(basename "$0") DESTINATION_DIRECTORY [i686|x86_64] [REPO_URL]"
   exit 2
-}
+fi
    
 DEST=$1
 ARCH=${2:-$DEFAULT_ARCH}
@@ -99,17 +98,16 @@ debug "create package directory: $PACKDIR"
 mkdir -p "$PACKDIR"
 
 LIST_HTML_FILE="$PACKDIR/core_os_$ARCH-index.html"
-test -s "$LIST_HTML_FILE" || { 
+if ! test -s "$LIST_HTML_FILE"; then 
   debug "fetch packages list: $REPO/"
   # Force trailing '/' needed by FTP servers.
   fetch -O "$LIST_HTML_FILE" "$REPO/" ||
     { debug "Error: cannot fetch packages list: $REPO"; exit 1; }
-}
+fi
 
 debug "packages HTML index: $LIST_HTML_FILE"
 LIST=$(< "$LIST_HTML_FILE" extract_href | awk -F"/" '{print $NF}' | sort -rn)
-test "$LIST" || 
-  { debug "Error: cannot process list file: $LIST_HTML_FILE"; exit 1; }  
+test "$LIST" || { debug "Error processing list file: $LIST_HTML_FILE"; exit 1; }  
 
 debug "create destination directory: $DEST"
 mkdir -p "$DEST"
@@ -119,10 +117,10 @@ for PACKAGE in ${BASIC_PACKAGES[*]}; do
   FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\)$")
   test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; exit 1; }
   FILEPATH="$PACKDIR/$FILE"
-  test -e "$FILEPATH" && check_compressed_integrity "$FILEPATH" || {
+  if ! test -e "$FILEPATH" || ! check_compressed_integrity "$FILEPATH"; then
     debug "download package: $REPO/$FILE"
     fetch -O "$FILEPATH" "$REPO/$FILE"
-  }
+  fi
   debug "uncompress package: $FILEPATH"
   uncompress "$FILEPATH" "$DEST"
 done
@@ -132,7 +130,7 @@ configure_pacman "$DEST" "$ARCH"
 
 debug "re-install basic packages and install extra packages: ${EXTRA_PACKAGES[*]}"
 chroot "$DEST" /usr/bin/pacman --noconfirm --arch $ARCH \
-  -Syf ${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}
+       -Syf ${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}
 
 debug "minimal configuration (DNS, passwd, hostname, mirrorlist, ...)" 
 configure_pacman "$DEST" "$ARCH"
