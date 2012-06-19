815788ea368effca2e3ded8817aaeb4a4aefc9b0
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2012-06-19 10:10:53 +0000

    [mediafire] comments

--- filesharing/mediafire/lib/mediafire.rb
+++ filesharing/mediafire/lib/mediafire.rb
@@ -12,6 +12,8 @@ module MediaFire
   def self.download(url)
     body, headers = catch { Curl.get_with_headers(url) }.
       on(Curl::Err::CurlError => NetworkError.new("Cannot get main page"))
+      
+    # Validate page
     headers["Location"] !~ /error.php/ or
       raise LinkError.new("Invalid or deleted file")
     doc = Nokogiri::HTML(body)
@@ -20,14 +22,16 @@ module MediaFire
     doc.at_css(".dl_options_innerblock") or
       raise ParseError.new("This does not seem a Mediafire file page", :body => body)
     
+    # Run obfuscated JS code to get the final file URL 
     script = doc.at_css(".dl_startlink script") or
       raise ParseError.new("Cannot find JS element", :body => body)
     js_stubs = "var document = {write: function(x) { return x; }};"
     link = catch { V8::Context.new.eval(js_stubs + script.text) }.
       on(V8::JSError => JSError.new("Error evaling JS", :body => body))
-    
     file_url = Nokogiri::HTML.fragment(link).at_css("a").maybe["href"] or
       raise ParseError.new("Cannot find link in obfuscated JS", :body => body)
+      
+    # Get the file name from the file URL and download it showing a nice progressbar
     file_name = CGI.unescape(File.basename(file_url))
     catch { Curl.download_with_progressbar(file_url, file_name) }.
       on(Curl::Err::CurlError => NetworkError.new("Error downloading file"))
