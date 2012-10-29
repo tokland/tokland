53cc204cfd456e6f8eb8039a9f459f21e677eed6
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2012-10-29 22:49:40 +0000

    main function

--- archlinux/arch-bootstrap.sh
+++ archlinux/arch-bootstrap.sh
@@ -4,7 +4,7 @@
 #
 # Dependencies: coreutils, wget, sed, gawk, tar, gzip, chroot, xz.
 # Bug tracker: http://code.google.com/p/tokland/issues
-# Author: Arnau Sanchez <tokland@gmail.com>
+# Contact: Arnau Sanchez <tokland@gmail.com>
 #
 # Install:
 #
@@ -33,12 +33,10 @@ extract_href() { sed -n '/<a / s/^.*<a [^>]*href="\([^\"]*\)".*$/\1/p'; }
 # Simple wrapper around wget
 fetch() { wget -c --passive-ftp --quiet "$@"; }
 
-### Main
-
 # Packages needed by pacman (BASIC_PACKAGES) are obtained this way:
 # 
 #   $ for PACKAGE in $(ldd /usr/bin/pacman | grep "=> /" | awk '{print $3}'); do 
-#       pacman -Qo $PACKAGE 
+#       pacman -Qo $PACKAGE
 #     done | awk '{print $5}' | sort -u | xargs
 BASIC_PACKAGES=(acl attr bzip2 expat glibc libarchive openssl pacman 
                 pacman-mirrorlist xz zlib curl gpgme libssh2 libassuan libgpg-error)
@@ -56,8 +54,7 @@ configure_pacman() {
 minimal_configuration() {
   local DEST=$1
   mkdir -p "$DEST/dev"
-  echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd"
-  # create root user (password: root)
+  echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd" 
   echo 'root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi/:14657::::::' > "$DEST/etc/shadow"
   touch "$DEST/etc/group"
   echo "bootstrap" > "$DEST/etc/hostname"
@@ -72,20 +69,20 @@ minimal_configuration() {
 check_compressed_integrity() {
   local FILEPATH=$1
   case "$FILEPATH" in
-  *.gz) gunzip -t "$FILEPATH";;
-  *.xz) xz -t "$FILEPATH";;
-  *) debug "Error: unknown package format: $FILEPATH"
-     return 1;;
+    *.gz) gunzip -t "$FILEPATH";;
+    *.xz) xz -t "$FILEPATH";;
+    *) debug "Error: unknown package format: $FILEPATH"
+       return 1;;
   esac
 }
 
 uncompress() {
   local FILEPATH=$1; local DEST=$2
   case "$FILEPATH" in
-  *.gz) tar xzf "$FILEPATH" -C "$DEST";;
-  *.xz) xz -dc "$FILEPATH" | tar x -C "$DEST";;
-  *) debug "Error: unknown package format: $FILEPATH"
-     return 1;;
+    *.gz) tar xzf "$FILEPATH" -C "$DEST";;
+    *.xz) xz -dc "$FILEPATH" | tar x -C "$DEST";;
+    *) debug "Error: unknown package format: $FILEPATH"
+       return 1;;
   esac
 }  
 
@@ -93,68 +90,70 @@ usage() {
   stderr "Usage: $(basename "$0") [-a i686|x86_64] [-r REPO_URL] DESTINATION_DIRECTORY"
 }
 
-### Main
+main() {
+  test $# -eq 0 && set -- "-h"
+  local ARCH=$DEFAULT_ARCH;
+  local REPO_URL=$DEFAULT_REPO_URL
+  while getopts "a:r:h" ARG; do
+    case "$ARG" in
+    a) ARCH=$OPTARG;;
+    r) REPO_URL=$OPTARG;;
+    *) usage; exit 1;;
+    esac
+  done
+  shift $(($OPTIND-1))
+  test $# -eq 1 || { usage; exit 1; }
+  local DEST=$1   
 
-test $# -eq 0 && set -- "-h"
-ARCH=$DEFAULT_ARCH;
-REPO_URL=$DEFAULT_REPO_URL
-while getopts "a:r:h" ARG; do
-  case "$ARG" in
-  a) ARCH=$OPTARG;;
-  r) REPO_URL=$OPTARG;;
-  *) usage; exit 1;;
-  esac
-done
-shift $(($OPTIND-1))
-test $# -eq 1 || { usage; exit 1; }
-DEST=$1   
+  local REPO="${REPO_URL%/}/core/os/$ARCH"
+  debug "using core repository: $REPO"
 
-REPO="${REPO_URL%/}/core/os/$ARCH"
-debug "using core repository: $REPO"
+  debug "create package directory: $PACKDIR"
+  mkdir -p "$PACKDIR"
 
-debug "create package directory: $PACKDIR"
-mkdir -p "$PACKDIR"
-
-LIST_HTML_FILE="$PACKDIR/core_os_$ARCH-index.html"
-if ! test -s "$LIST_HTML_FILE"; then 
-  debug "fetch packages list: $REPO/"
-  # Force trailing '/' needed by FTP servers.
-  fetch -O "$LIST_HTML_FILE" "$REPO/" ||
-    { debug "Error: cannot fetch packages list: $REPO"; exit 1; }
-fi
-
-debug "packages HTML index: $LIST_HTML_FILE"
-LIST=$(< "$LIST_HTML_FILE" extract_href | awk -F"/" '{print $NF}' | sort -rn)
-test "$LIST" || 
-  { debug "Error processing list file: $LIST_HTML_FILE"; exit 1; }  
-
-debug "create destination directory: $DEST"
-mkdir -p "$DEST"
-
-debug "pacman package and dependencies: ${BASIC_PACKAGES[*]}"
-for PACKAGE in ${BASIC_PACKAGES[*]}; do
-  FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\)$") ||
-    { debug "Error: cannot find package: $PACKAGE"; exit 1; }
-  FILEPATH="$PACKDIR/$FILE"
-  if ! test -e "$FILEPATH" || ! check_compressed_integrity "$FILEPATH"; then
-    debug "download package: $REPO/$FILE"
-    fetch -O "$FILEPATH" "$REPO/$FILE"
+  local LIST_HTML_FILE="$PACKDIR/core_os_$ARCH-index.html"
+  if ! test -s "$LIST_HTML_FILE"; then 
+    debug "fetch packages list: $REPO/"
+    # Force trailing '/' needed by FTP servers.
+    fetch -O "$LIST_HTML_FILE" "$REPO/" ||
+      { debug "Error: cannot fetch packages list: $REPO"; exit 1; }
   fi
-  debug "uncompress package: $FILEPATH"
-  uncompress "$FILEPATH" "$DEST"
-done
 
-debug "configure DNS and pacman"
-configure_pacman "$DEST" "$ARCH"
+  debug "packages HTML index: $LIST_HTML_FILE"
+  local LIST=$(< "$LIST_HTML_FILE" extract_href | awk -F"/" '{print $NF}' | sort -rn)
+  test "$LIST" || 
+    { debug "Error processing list file: $LIST_HTML_FILE"; exit 1; }  
 
-debug "re-install basic packages and install extra packages: ${EXTRA_PACKAGES[*]}"
-minimal_configuration "$DEST"
-LC_ALL=C chroot "$DEST" /usr/bin/pacman --noconfirm --arch $ARCH \
-  -Syf ${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}
+  debug "create destination directory: $DEST"
+  mkdir -p "$DEST"
 
-debug "minimal configuration (DNS, passwd, hostname, mirrorlist, ...)" 
-configure_pacman "$DEST" "$ARCH"
+  debug "pacman package and dependencies: ${BASIC_PACKAGES[*]}"
+  for PACKAGE in ${BASIC_PACKAGES[*]}; do
+    local FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\)$")
+    test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; exit 1; }
+    local FILEPATH="$PACKDIR/$FILE"
+    if ! test -e "$FILEPATH" || ! check_compressed_integrity "$FILEPATH"; then
+      debug "download package: $REPO/$FILE"
+      fetch -O "$FILEPATH" "$REPO/$FILE"
+    fi
+    debug "uncompress package: $FILEPATH"
+    uncompress "$FILEPATH" "$DEST"
+  done
 
-echo "Done! you can now use the system: chroot \"$DEST\""
-echo "Note that some applications may require directories /dev, /proc or /sys to be mounted:"
-echo "  $ sudo mount --bind /dev \"$DEST/dev\""
+  debug "configure DNS and pacman"
+  configure_pacman "$DEST" "$ARCH"
+
+  debug "re-install basic packages and install extra packages: ${EXTRA_PACKAGES[*]}"
+  minimal_configuration "$DEST"
+  LC_ALL=C chroot "$DEST" /usr/bin/pacman --noconfirm --arch $ARCH \
+    -Syf ${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}
+
+  debug "minimal configuration (DNS, passwd, hostname, mirrorlist, ...)" 
+  configure_pacman "$DEST" "$ARCH"
+
+  echo "Done! you can now use the system: chroot \"$DEST\""
+  echo "Note that some applications may require /dev /proc or /sys to be mounted:"
+  echo "  $ sudo mount --bind /dev \"$DEST/dev\""
+}
+
+main "$@"
