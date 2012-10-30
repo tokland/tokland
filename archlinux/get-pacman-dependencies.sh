f308bcf36c6678959b830d5120ca038e9966d8b7
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2012-10-30 22:25:06 +0000

    ad get-pacman-dependencies.sh

---
+++ archlinux/get-pacman-dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e -u -o pipefail
+
+# shared library dependencies
+shared_dependencies() {
+  local EXECUTABLE=$1
+  for PACKAGE in $(ldd "$EXECUTABLE" | grep "=> /" | awk '{print $3}'); do 
+    LC_ALL=c pacman -Qo $PACKAGE
+  done | awk '{print $5}'
+}
+
+# dependencies from PKGBUILD
+pkgbuild_dependencies() {
+  local PKGBUILD=$1
+  local EXCLUDE=$2
+  source "$PKGBUILD"
+  for DEPEND in ${depends[@]}; do
+    echo "$DEPEND" | sed "s/[>=<].*$//"
+  done | grep -v "$EXCLUDE"
+}
+
+{ 
+  shared_dependencies "/usr/bin/pacman"
+  pkgbuild_dependencies "/var/abs/core/pacman/PKGBUILD" "bash"
+} | sort -u | xargs
