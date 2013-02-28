bb313fa15d9ccafa5040526a1a4d16b73a20c58d
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2013-02-28 13:56:20 +0000

    more

--- lavanguardia/process_pdf.sh
+++ lavanguardia/process_pdf.sh
@@ -21,10 +21,10 @@ for PDF in "$@"; do
       <script>
         window.addEventListener('load', function() {
           var fen = '$FEN';
+          var white_to_play = fen.split(" ")[1] == 'w';
           setTimeout(function() {
             var frame = document.getElementById('board').contentWindow;
             frame.setLevel(2);
-            var white_to_play = fen.split(" ")[1] == 'w';
             frame.newGame(!white_to_play);
             frame.p.SetFEN(fen);
           }, 50);
